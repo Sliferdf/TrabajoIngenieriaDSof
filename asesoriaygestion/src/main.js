1c92ed8b46b4531f7379a1f07434bc4775a62c3a
sliferdf <dbnaer71@gmail.com>
2019-09-18 19:16:14 -0500

    loginv1

--- asesoriaygestion/src/main.js
+++ asesoriaygestion/src/main.js
@@ -10,6 +10,22 @@ Vue.use(BootstrapVue)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import * as firebase from "firebase";       
+import VueFirebase from 'vue-firebase'
+
+// Config firebase project
+const firebaseConfig = {
+  apiKey: "AIzaSyA9iNcxUs8OynOfP8MgnAA22l5Symr83rY",
+  authDomain: "logiinvue.firebaseapp.com",
+  databaseURL: "https://logiinvue.firebaseio.com",
+  projectId: "logiinvue",
+  storageBucket: "logiinvue.appspot.com",
+  messagingSenderId: "28025408729",
+  appId: "1:28025408729:web:00c924f378d799864182c8"
+};
+
+Vue.use(VueFirebase, {firebase: firebase, config: FBCONFIG});
+
 Vue.config.productionTip = false
 
 new Vue({
